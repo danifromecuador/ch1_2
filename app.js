fa275afc23a093d0b3583830305d6c0de9845dc0
dani <danifromecuador@gmail.com>
2025-02-05 22:43:35 -0500

    feat: manage GET response

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const http = require('http')
 const fs = require('fs')
+let data = {}
 
-const data = fs.readFile('databasde.json', 'utf8', (err, pl)=>{
-  if (err) console.log(404)
-  else console.log(200)
+
+fs.readFile('./databsase.json', 'utf8', (err, pl) => {
+  err ? data = "404" : data = pl
 })
 
-console.log(data)
+const server = http.createServer((req, res) => {
+  handleGET(req, res)
+})
+
+const handleGET = (req, res) => {
+  data === '404'
+    ? res.end('data not found')
+    : res.end(data)
+}
+
+
+server.listen(3000, () => console.log("Server is running on localhost:3000"))
